1dbd864bf09af25223dbcbf044a3c6bb729b1da5
Glindley23 <graham.lindley23@gmail.com>
2022-12-18 11:03:30 -0500

    First commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 )
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true})) 
 
 // ROUTES
 app.get('/', (req, res) => {
